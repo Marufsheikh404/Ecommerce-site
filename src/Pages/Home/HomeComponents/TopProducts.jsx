a2e547a547f027e84ddad21cc609ac29b8e4ce8f
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-10 22:53:33 +0600

    new arrival is comolete

--- src/Pages/Home/HomeComponents/TopProducts.jsx
+++ src/Pages/Home/HomeComponents/TopProducts.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SharedComponents/SectionTitle';
+import useData from '../../../Hooks/useData';
+import ProductCard from '../../../Components/SharedComponents/ProductCard';
 
 const TopProducts = () => {
+    const {products} = useData();
     return (
-        <div className='container mx-auto px-4 my-3'>
-           <SectionTitle title={'New'} colorTitle={'Arrivals'} des={'Shop online for new arrivals and get free shipping!'}></SectionTitle>
-        </div>
+        <>
+            <div className='container mx-auto px-4 my-3'>
+                <SectionTitle title={'New'} colorTitle={'Arrivals'} des={'Shop online for new arrivals and get free shipping!'}></SectionTitle>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+                {
+                    products
+                    .slice(-8)
+                    .map(p=> <ProductCard key={p?.id} products ={p}></ProductCard>)
+                }
+            </div>
+        </>
     );
 };
 
