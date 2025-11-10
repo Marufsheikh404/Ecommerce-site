d978c9eec7ec26cc43b760bb0e7351686d888321
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-10 16:06:43 +0600

    border is done

--- src/Components/SharedComponents/Header/Navber.jsx
+++ src/Components/SharedComponents/Header/Navber.jsx
@@ -5,19 +5,34 @@ import { NavLink } from 'react-router';
 
 const Navber = () => {
     return (
-        <div className='flex items-center justify-between container mx-auto px-4'>
-            <div className='flex items-center gap-2 cp py-2 px-2 rounded-md text-white'>
+        <div className='flex items-center justify-between container mx-auto px-3 border-y border-gray-200 py-5'>
+            <div className='flex items-center gap-1 cp py-2 px-2 rounded-md text-white'>
                 <BiCategory size={25} />
                 <p>All Category</p>
             </div>
             <div className='flex items-center gap-15 cts'>
-                <NavLink to="/" style={({ isActive }) => ({color: isActive ? '#5CAF90' : ''})}>Home</NavLink>
-                <NavLink to="/" style={({ isActive }) => ({color: isActive ? '#5CAF90' : ''})}>Category</NavLink>
-                <NavLink to="/" style={({ isActive }) => ({color: isActive ? '#5CAF90' : ''})}>Blogs</NavLink>
-                <NavLink to="/" style={({ isActive }) => ({color: isActive ? '#5CAF90' : ''})}>Products</NavLink>
-                <NavLink to="/" style={({ isActive }) => ({color: isActive ? '#5CAF90' : ''})}>Pages</NavLink>
+                <NavLink to="/"
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#5CAF90] font-semibold' : 'text-gray-700'
+                    }>Home</NavLink>
+                <NavLink to="/category"
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#5CAF90] font-semibold' : 'text-gray-700'
+                    }>Category</NavLink>
+                <NavLink to="/blogs"
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#5CAF90] font-semibold' : 'text-gray-700'
+                    }>Blogs</NavLink>
+                <NavLink to="/products"
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#5CAF90] font-semibold' : 'text-gray-700'
+                    }>Products</NavLink>
+                <NavLink to="/pages"
+                    className={({ isActive }) =>
+                        isActive ? 'text-[#5CAF90] font-semibold' : 'text-gray-700'
+                    }>Pages</NavLink>
             </div>
-            <div className='flex items-center gap-2 cp py-2 px-2 rounded-md text-white'>
+            <div className='flex items-center gap-1 cp py-2 px-2 rounded-md text-white'>
                 <CiLocationOn />
                 <p>New York</p>
             </div>
