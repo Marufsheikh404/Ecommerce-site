f4e9837c9c551e9cdc04eb470cac3e18ef2a2481
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-10 20:22:49 +0600

    useHook

--- src/Components/SharedComponents/SectionTitle.jsx
+++ src/Components/SharedComponents/SectionTitle.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import useData from '../../Hooks/useData';
 
 const SectionTitle = ({ title, colorTitle, des }) => {
+    const { categories } = useData();
     return (
-        <div className='leading-1'>
-            <div className='flex items-center text-2xl font-bold gap-4'>
-                <h1>{title}</h1> <h1 className='text-[#5CAF90]'>{colorTitle}</h1>
+        <div className='flex justify-between w-full'>
+            <div className='leading-1'>
+                <div className='flex items-center text-2xl font-bold gap-4'>
+                    <h1>{title}</h1> <h1 className='text-[#5CAF90]'>{colorTitle}</h1>
+                </div>
+                <p className='text-xs text-gray-500'>{des}</p>
             </div>
-            <p className='text-xs text-gray-500'>{des}</p>
+            <div className='flex items-center gap-4'>
+                {
+                    categories.map(item=>{
+                      return <p>{item.name}</p>
+                    })
+                }
+            </div>
         </div>
     );
 };
