562567872050dd45d9a952078269839d5b9449ba
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-13 00:05:06 +0600

    home category responsive is done

--- src/Components/SharedComponents/ProductCard.jsx
+++ src/Components/SharedComponents/ProductCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 
 const ProductCard = ({ products }) => {
     return (
-        <div className="container mx-auto px-4 flex-1 card bg-base-100 w-[230px] h-[300px] shadow-md hover:shadow-2xl transition-all duration-300 cursor-pointer">
+        <div className="container mx-auto px-4 flex-1 card bg-base-100 lg:w-[230px] lg:h-[300px] shadow-md hover:shadow-2xl transition-all duration-300 cursor-pointer">
             <figure className="h-[150px]">
                 <img
                     src={products.image}
@@ -31,7 +31,7 @@ const ProductCard = ({ products }) => {
                             <IoIosStarHalf />
                         </p>
                     </div>
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-around lg:justify-between">
                         <p className="font-bold text-sm text-[#4B5966]">${products.price}</p>
                         <p className="text-gray-400 line-through font-semibold text-xs -translate-x-8">${products.mrp}</p>
                     </div>
