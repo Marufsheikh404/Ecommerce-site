4ddd8fd2bc315af7880fbc673c905e2484ac8d18
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-12 23:53:12 +0600

    home category responsive is done

--- src/Pages/Home/HomeComponents/HomoCategory.jsx
+++ src/Pages/Home/HomeComponents/HomoCategory.jsx
@@ -4,15 +4,15 @@ import useData from '../../../Hooks/useData';
 const HomoCategory = () => {
     const {categories} = useData();
     return (
-        <div className='flex items-center gap-5 my-4 container mx-auto px-4'>
+        <div className='flex flex-col lg:flex-row items-center gap-5 my-4 container mx-auto px-4'>
             {
                 categories.map(category => (
-                    <div key={category.id} className="bg-gray-200 w-96 shadow-sm rounded-lg">
-                        <figure className="px-10 pt-10">
+                    <div key={category.id} className="bg-gray-200 lg:w-96 w-90 shadow-sm rounded-lg">
+                        <figure className="lg:px-10 lg:pt-10 mt-4 lg:mt-0">
                             <img
                                 src={category.image}
                                 alt="Shoes"
-                                className="rounded-xl w-16 h-10 object-contain hover:-translate-y-3 transition-all duration-300" />
+                                className="rounded-xl w-16 lg:h-10 h-8 translate-x-38 lg:translate-x-0 object-cover lg:object-contain hover:-translate-y-3 transition-all duration-300" />
                         </figure>
                         <div className="card-body items-center text-center">
                             <h2 className="card-title text-sm">{category.name}</h2>
