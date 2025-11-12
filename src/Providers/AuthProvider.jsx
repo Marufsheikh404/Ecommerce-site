02dcbee3478906a41b79f974037aa86ad528c76a
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-13 01:19:40 +0600

    firebase is added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { AuthContext } from '../Context/AuthContext';
+import { auth } from '../firebase/firebase.config';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 
 const AuthProvider = ({children}) => {
-    const Info ={
+    const [user,setUser] = useState('');
+    const [loading,setLoading] = useState(false);
 
+    // signIn user
+    const signIn = (email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email,password)
+    };
+
+    // signUp user 
+    const signUp = (email, password)=>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    };
+
+    // SignOut
+    const LogOut =()=>{
+        setLoading(true)
+        return signOut(auth);
+    };
+    const Info ={
+        loading,
+        setLoading,
+        signIn,
+        signUp,
+        LogOut,
+        user
     }
+
+    useEffect(()=>{
+        const unsubcribe = onAuthStateChanged(auth,(currentUser)=>{
+            console.log('currenUser', currentUser)
+            setUser(currentUser)
+        })
+        return ()=>{
+            unsubcribe();
+        }
+    },[]);
     return (
         <AuthContext.Provider value={Info}>
             {children}
