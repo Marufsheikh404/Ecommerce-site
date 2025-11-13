6cfed19df4a88337f7c141bf38d18a56887d716f
MD.MARUF SHEIKH <marufsheikh803@gmail.com>
2025-11-13 19:36:11 +0600

    firebase added successfully

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,13 @@
 import Lottie from 'lottie-react';
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import login from '../../Animation/login.json'
 import useAuth from '../Hook/useAuth';
 import Swal from 'sweetalert2';
 
 const Login = () => {
     const { signIn, setLoading } = useAuth();
+    const navigate = useNavigate();
     const handleClick = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
@@ -16,6 +17,8 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user)
+                e.target.reset();
+                navigate('/')
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
@@ -38,7 +41,7 @@ const Login = () => {
             });
     }
     return (
-        <div className='container mx-auto px-4'>
+        <div className='container mx-auto px-4 flex flex-col lg:flex-row items-center justify-center gap-15'>
             <div>
                 <form onSubmit={handleClick} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend text-xl text-center text-[#5CAF90] font-semibold">SignIn</legend>
@@ -55,7 +58,7 @@ const Login = () => {
                     <h1 className='cts translate-x-2'> Do Not Have Any Account? Please <Link to={'/register'}><span className='text-[#5CAF90] font-semibold'>Register</span></Link></h1>
                 </div>
             </div>
-            <div>
+            <div className='w-96'>
                 <Lottie animationData={login}></Lottie>
             </div>
         </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import useAuth from '../Hook/useAuth';
 import Swal from 'sweetalert2';
+import Lottie from 'lottie-react';
+import animation from '../../Animation/Appointment booking with smartphone.json'
 
 const Register = () => {
     const { signUp, setLoading } = useAuth();
+    const navigate = useNavigate();
     const handleClick = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
@@ -13,6 +16,8 @@ const Register = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user)
+                e.target.reset();
+                navigate('/')
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
@@ -35,16 +40,16 @@ const Register = () => {
             });
     };
     return (
-        <div className='container mx-auto px-4'>
+        <div className='container mx-auto px-4 flex flex-col lg:flex-row items-center justify-center gap-15'>
             <div>
                 <form onSubmit={handleClick} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
                     <legend className="fieldset-legend text-xl text-center text-[#5CAF90] font-semibold">Register</legend>
 
                     <label className="label">First Name</label>
-                    <input name='first' type="email" className="input" placeholder="Enter Your First Name" />
+                    <input name='first' type="text" className="input" placeholder="Enter Your First Name" />
 
                     <label className="label">Last Name</label>
-                    <input name='last' type="email" className="input" placeholder="Enter You Last Name" />
+                    <input name='last' type="text" className="input" placeholder="Enter You Last Name" />
 
                     <label className="label">Email</label>
                     <input name='email' type="email" className="input" placeholder="Email" />
@@ -57,6 +62,9 @@ const Register = () => {
                 <div>
                     <h1 className='cts translate-x-2'>You Already Have An Account?Please <Link to={'/login'}><span className='text-[#5CAF90] font-semibold'>Login</span></Link></h1>
                 </div>
+            </div>
+            <div className='w-96'>
+                <Lottie animationData={animation}></Lottie>
             </div>
         </div>
     );
